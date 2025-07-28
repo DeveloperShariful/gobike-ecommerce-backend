53b5dd59cbce6e40e7b83f3c0bc0bc70d113e83d
DeveloperShariful <si9504460@gmail.com>
2025-07-28 15:31:41 +0600

    Configure PostgreSQL for Render

--- backend/settings.py
+++ backend/settings.py
@@ -79,12 +79,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'backend.wsgi.application'
 
 
-# Database
+import os
+import dj_database_url
+
+# ...
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    )
 }
 
 
